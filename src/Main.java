9cb5bc81cceba641421a560282ec3e964bf9122e
Alexandra Gorodnitskaya <as.gorodnitskaya@gmail.com>
2024-08-29 18:57:06 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Sasha");
+        System.out.println(5+10);
+        System.out.println((72-14)*7);
+        System.out.println("    +");
+        System.out.println("   +++");
+        System.out.println("  +++++");
+        System.out.println(" +++++++");
+        System.out.println("+++++++++");
+        System.out.println("   +++");
+        System.out.println("   +++");
+    }
+}
